85924eb441d7a24920d48e2cc35ed0e07dbbc59e
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-22 23:12:29 -0500

    I give up on google

--- code.js
+++ code.js
@@ -1,30 +1,30 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-var lastResponse;
+// var lastResponse;
 
-function decodeJwtResponse(cred) {
-    return jwt.decode(cred)
-}
+// function decodeJwtResponse(cred) {
+//     return jwt.decode(cred)
+// }
 
-function handleCredentialResponse(response) {
-    // decodeJwtResponse() is a custom function defined by you
-    // to decode the credential response.
-    const responsePayload = decodeJwtResponse(response.credential);
-    lastResponse = responsePayload;
-    logData(responsePayload);
-}
+// function handleCredentialResponse(response) {
+//     // decodeJwtResponse() is a custom function defined by you
+//     // to decode the credential response.
+//     const responsePayload = decodeJwtResponse(response.credential);
+//     lastResponse = responsePayload;
+//     logData(responsePayload);
+// }
 
-function logData(responsePayload) {
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
-}
+// function logData(responsePayload) {
+//     console.log("ID: " + responsePayload.sub);
+//     console.log('Full Name: ' + responsePayload.name);
+//     console.log('Given Name: ' + responsePayload.given_name);
+//     console.log('Family Name: ' + responsePayload.family_name);
+//     console.log("Image URL: " + responsePayload.picture);
+//     console.log("Email: " + responsePayload.email);
+// }
 
-function testLog() {
-    logData(lastResponse);
-}
+// function testLog() {
+//     logData(lastResponse);
+// }
 
 console.log("Hello! code.js loaded!");
