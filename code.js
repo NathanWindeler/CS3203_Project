1db020d583194ece56e84cf82442c3e7ace45d1d
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-23 04:47:47 -0500

    Added cookies counting number of times clicked

--- code.js
+++ code.js
@@ -27,4 +27,35 @@
 //     logData(lastResponse);
 // }
 
+// https://www.w3schools.com/js/js_cookies.asp
+
+let numClicks = 0;
+
+function loadClicks(){
+    let num = document.cookie
+    if (num != ""){
+        numClicks = num;
+        updateClicksText();
+    }
+}
+
+
+function updateClicks(){
+    numClicks++;
+    updateClicksText();
+    saveClicks();
+}
+
+function updateClicksText(){
+    document.getElementById("clickCounter").textContent = ` Times clicked: ${numClicks}`
+}
+
+function saveClicks(){
+    document.cookie = numClicks
+}
+
+function loadClicks(){
+    numClicks = document.cookie
+}
+
 console.log("Hello! code.js loaded!");
