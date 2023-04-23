9a953de8ee961e993b05c7246599b7c5eebfeef1
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-22 20:21:13 -0500

    Removed comments

--- code.js
+++ code.js
@@ -1,36 +1,6 @@
-import * as jose from 'jose'
 import jwt from 'jsonwebtoken';
 
-// const publicKey = '';
-
-// const decrypted = jwt.verify(
-//     token, publicKey,
-//     {algorithms: ['ES512'],
-//     audience: 'https://domain.tld',
-//     issuer: 'https://domain.tld' }
-// );
-
-//https://betterprogramming.pub/jwt-ultimate-how-to-guide-with-best-practices-in-javascript-f7ba4c48dfbd
-
-// function decodeJwtResponse(cred) {
-//     // const decoder = new TextDecoder();
-//     token = cred;
-//     return jwt.verify(
-//         token, publicKey,
-//         {algorithms: ['ES512'],
-//         audience: 'https://domain.tld',
-//         issuer: 'https://domain.tld' }
-//     );
-// }
-
-//https://googleapis.dev/python/google-auth/1.11.3/reference/google.auth.jwt.html
-
-// https://stackoverflow.com/questions/72546348/google-sign-in-decoding-jwt-response
-
-
 var lastResponse;
-
-console.log('')
 
 function decodeJwtResponse(cred) {
     return jwt.decode(cred)
