ecf7826a73eef42a6cd6e487122b04f80fd51d97
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-22 20:10:38 -0500

    Attempt 2

--- code.js
+++ code.js
@@ -29,6 +29,10 @@ import jwt from 'jsonwebtoken';
 // https://stackoverflow.com/questions/72546348/google-sign-in-decoding-jwt-response
 import jwt from "jsonwebtoken";
 
+var lastResponse;
+
+console.log('')
+
 function decodeJwtResponse(cred) {
     return jwt.decode(cred)
 }
@@ -37,13 +41,21 @@ function handleCredentialResponse(response) {
     // decodeJwtResponse() is a custom function defined by you
     // to decode the credential response.
     const responsePayload = decodeJwtResponse(response.credential);
+    lastResponse = responsePayload;
+    logData(responsePayload);
+}
 
+function logData(responsePayload) {
     console.log("ID: " + responsePayload.sub);
     console.log('Full Name: ' + responsePayload.name);
     console.log('Given Name: ' + responsePayload.given_name);
     console.log('Family Name: ' + responsePayload.family_name);
     console.log("Image URL: " + responsePayload.picture);
     console.log("Email: " + responsePayload.email);
+}
+
+function testLog() {
+    logData(lastResponse);
 }
 
 console.log("Hello! code.js loaded!");
