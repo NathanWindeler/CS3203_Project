0c2bfaa9dfbca12b3dbe417d885483ba95660333
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-24 17:20:59 -0500

    Remove google code and begin work on game

--- code.js
+++ code.js
@@ -1,32 +1,3 @@
-// import jwt from 'jsonwebtoken';
-
-// var lastResponse;
-
-// function decodeJwtResponse(cred) {
-//     return jwt.decode(cred)
-// }
-
-// function handleCredentialResponse(response) {
-//     // decodeJwtResponse() is a custom function defined by you
-//     // to decode the credential response.
-//     const responsePayload = decodeJwtResponse(response.credential);
-//     lastResponse = responsePayload;
-//     logData(responsePayload);
-// }
-
-// function logData(responsePayload) {
-//     console.log("ID: " + responsePayload.sub);
-//     console.log('Full Name: ' + responsePayload.name);
-//     console.log('Given Name: ' + responsePayload.given_name);
-//     console.log('Family Name: ' + responsePayload.family_name);
-//     console.log("Image URL: " + responsePayload.picture);
-//     console.log("Email: " + responsePayload.email);
-// }
-
-// function testLog() {
-//     logData(lastResponse);
-// }
-
 // https://www.w3schools.com/js/js_cookies.asp
 
 let numClicks = 0;
@@ -47,15 +18,20 @@ function updateClicks(){
 }
 
 function updateClicksText(){
-    document.getElementById("clickCounter").textContent = ` Times clicked: ${numClicks}`
+    document.getElementById("clickCounter").textContent = ` Times clicked: ${numClicks}`;
 }
 
 function saveClicks(){
-    document.cookie = numClicks
+    document.cookie = numClicks;
 }
 
 function loadClicks(){
-    numClicks = document.cookie
+    numClicks = document.cookie;
 }
+
+// LOAD ITEM BUTTONS
+var buttonsDiv = document.getElementById("buttonsDiv");
+fetch('./items.json')
+
 
 console.log("Hello! code.js loaded!");
