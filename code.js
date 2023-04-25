f42eaede3e4dc9c9dce955b293fc2ae3e9e99fb3
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-04-24 20:08:05 -0500

    Game playable, use dev console to see output

--- code.js
+++ code.js
@@ -44,8 +44,11 @@ function createButtons(){
         newButton = document.createElement("button");
         newImage = document.createElement("img");
         newImage.src = "images/" + itemsArray[i] + ".png";
-        newImage.width = "32";
-        newImage.height = "32";
+        newImage.width = "48";
+        newImage.height = "48";
+        newImage.id = itemsArray[i];
+
+        newImage.addEventListener("click", guess);
 
         newButton.appendChild(newImage);
         buttonsDiv.appendChild(newButton);
@@ -53,8 +56,94 @@ function createButtons(){
 
 }
 
+var recipesArray = null;
+var revealedArray = null;
+async function loadRecipes(){
+    // fetch('https://api.chucknorris.io/jokes/random?category=dev')
+    // fetch('./recipes.json')
+    // .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
+    // .then(data => console.log(data));
+
+    const response = await fetch('./recipes.json');
+    const recipesJson = await response.json();
+
+    console.log(recipesJson);
+    recipesArray = recipesJson;
+    
+    for(i = 0; i < recipesArray.recipes.length; i++){
+        console.log(recipesArray.recipes[i]);
+    }
+
+    reset();
+}
+
+function chooseRecipe(){
+    chosenRecipe = recipesArray.recipes[Math.floor(Math.random() * 4)]
+}
+
+function recipeContains(item){
+    for(i = 0; i < chosenRecipe.recipeArr.length; i++){
+        if(chosenRecipe.recipeArr[i] == item){
+            return true;
+        }
+    }
+    return false;
+}
+
+function guess(element){
+    console.log(element);
+    console.log(element.target.id)
+    
+    item = element.target.id;
+
+    if(recipeContains(item)){
+        // Correct Guess
+        console.log("Correct guess!")
+        revealItem(item);
+        checkWin();
+    } else {
+        // Incorrect guess
+    }
+}
+
+function revealItem(item){
+    for(i = 0; i < chosenRecipe.recipeArr.length; i++){
+        if(chosenRecipe.recipeArr[i] == item){
+            document.getElementById("crafting" + i).src = `images/${item}.png`;
+            revealedArray[i] = chosenRecipe.recipeArr[i];
+        }
+    }
+}
+
+function arraysEqual(ray1, ray2){
+    for(i = 0; i < ray1.length && i < ray2.length; i++){
+        if(ray1[i] != ray2[i]){
+            return false;
+        }
+    }
+    return (ray1.length == ray2.length);
+}
+
+function checkWin(){
+    if(arraysEqual(revealedArray, chosenRecipe.recipeArr)){
+        console.log(`The recipe is: ${chosenRecipe.recipeID}! You win!`)
+    }
+}
+
+function reset(){
+    // Blank crafting arrays
+    revealedArray = ["blank", "blank", "blank", "blank", "blank", "blank", "blank", "blank", "blank"]
+    for(i = 0; i < 9; i++){
+        document.getElementById("crafting" + i).src = `images/blank.png`;
+    }
+    // Choose recipe
+    chooseRecipe();
+    console.log("Game reset! Recipe chosen: " + chosenRecipe.recipeID)
+}
+
 window.onload = function(){
     createButtons();
+    loadRecipes();
 }
 
 console.log("Hello! code.js loaded!");
